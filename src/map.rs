4dab8bcaae6ece5ae5e6888b19b403d17edf2ada
Wilmer Finol <wofinoli@gmail.com>
2020-06-17 12:28:49 -0500

    Add Map struct and constructor
    Added Map struct to represent each map, along with constructor that can
    read tile data from a hard coded two-dimensional vector of u32s. Each
    Map has a name.

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,14 @@
-pub fn works() {
-    println!("this works");
+#[derive(Debug)]
+pub struct Map {
+    tiles: Vec<Vec<u32>>,
+    name: String,
+}
+
+impl Map {
+    pub fn new(tiles: Vec<Vec<u32>>, name: String) -> Map {
+        Map {
+            tiles,
+            name,
+        }
+    }
 }
