bd54b51d0b14c58d49524af3f32f312b53262ac4
Wilmer Finol <wofinoli@gmail.com>
2020-06-17 17:57:13 -0500

    Add map file reading capability
    Added a generate_tiles, a method on the Map struct that handles reading
    a map from a file. Robust error handling is not implemented yet. Will
    add map printer before adding to generate_tiles in order to make
    debugging easier.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,7 @@ mod map;
 use map::Map;
 
 pub fn run() {
-    let tiles = vec![ vec![1, 2, 3, 4, 5], 
-                      vec![6, 7, 8, 9, 10]];
 
-
-    let level = Map::new(tiles, String::from("Level 1"));
+    let level = Map::new(String::from("Level 1"));
     println!("{:?}",level);
 }
