b906e10a9725ffc8e9cf9f2a36db88703f639b7b
Wilmer Finol <wofinoli@gmail.com>
2020-06-17 12:28:49 -0500

    Add Map struct and constructor
    Added Map struct to represent each map, along with constructor that can
    read tile data from a hard coded two-dimensional vector of u32s. Each
    Map has a name.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 mod map;
 
+use map::Map;
+
 pub fn run() {
-    map::works();
+    let tiles = vec![ vec![1, 2, 3, 4, 5], 
+                      vec![6, 7, 8, 9, 10]];
+
+
+    let level = Map::new(tiles, String::from("Level 1"));
+    println!("{:?}",level);
 }
