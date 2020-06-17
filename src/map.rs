5372bb221049c0fe046ac37e2291f80be3c22fa8
Wilmer Finol <wofinoli@gmail.com>
2020-06-17 18:22:34 -0500

    Refactor generate_tiles to find start and end
    Refactored generate_tiles method to make sure start and end points are
    found.

--- src/map.rs
+++ src/map.rs
@@ -11,37 +11,46 @@ pub struct Map {
 impl Map {
     pub fn new(filename: String) -> Map {
 
-        let level_name = "src/levels/test.txt";
+        let level_name = format!("src/levels/{}", filename);
         let tile_string = fs::read_to_string(level_name)
             .expect("Something went wrong reading the file.");
 
-        let tiles = Map::generate_tiles(tile_string);
+        let (start, end, tiles) = Map::generate_tiles(tile_string);
 
         Map {
             tiles,
-            name: String::from("Test"),
-            start: (0,0),
-            end: (0,0),
+            name: filename[..filename.len()-4].to_string(),
+            start,
+            end,
         }
     }
 
     // TODO: Add proper error handling
-    fn generate_tiles(tile_string: String) -> Vec<Vec<u32>> {
+    fn generate_tiles(tile_string: String) -> 
+        ( (u32, u32), (u32, u32), Vec<Vec<u32>>) {
         let mut tiles: Vec<Vec<u32>> = vec![];
+        let mut start: (u32, u32) = (0, 0);
+        let mut end: (u32, u32) = (0, 0);
 
-        for line in tile_string.lines() {
+        for (row, line) in tile_string.lines().enumerate() {
             let mut tile_line: Vec<u32> = vec![];
-            for tile in line.chars() {
+            for (col, tile) in line.chars().enumerate() {
                 match tile.to_digit(10) {
-                    Some(d) => tile_line.push(d),
+                    Some(d) => {
+                        tile_line.push(d);
+                        if d == 2 {
+                            start = (row as u32, col as u32);
+                        } else if d == 3 {
+                            end = (row as u32, col as u32);
+                        }
+                    }
                     None => tile_line.push(0),
                 };
             }
 
             tiles.push(tile_line);
                 
-            println!("{:?}", tiles);
         }
-        vec![vec![0]]
+        (start,end,tiles)
     }
 }
