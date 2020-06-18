71b1296306ae2b6b7d3ce1d4b4fd2d9d4b80e302
Wilmer Finol <wofinoli@gmail.com>
2020-06-17 19:34:02 -0500

    Add error handling to generate_tiles
    Refactors the Map struct, its constructor, and its geneate_tiles method
    to take advantage of the Position struct to make error handling easier.
    Position struct also makes code more readable.

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::error::Error;
 
 #[derive(Debug)]
 struct Position {
@@ -6,50 +7,63 @@ struct Position {
     y: f64,
 }
 
+impl Position {
+    pub fn new(x: f64, y: f64) -> Position {
+        Position { x, y }
+    }
+}
+
 #[derive(Debug)]
 pub struct Map {
     tiles: Vec<Vec<u32>>,
     name: String,
-    start: (u32, u32),
-    end: (u32, u32),
+    start: Position,
+    goal: Position,
 }
 
 impl Map {
-    pub fn new(filename: String) -> Map {
+    pub fn new(filename: String) -> Result<Map, Box<dyn Error>> {
 
         let level_name = format!("src/levels/{}", filename);
         let tile_string = fs::read_to_string(level_name)
             .expect("Something went wrong reading the file.");
 
-        let (start, end, tiles) = Map::generate_tiles(tile_string);
+        let map_data = Map::generate_tiles(tile_string)?;
 
-        Map {
-            tiles,
+        Ok(Map {
+            tiles: map_data.0,
             name: filename[..filename.len()-4].to_string(),
-            start,
-            end,
-        }
+            start: map_data.1,
+            goal: map_data.2,
+        })
     }
 
-    // TODO: Add proper error handling
     fn generate_tiles(tile_string: String) -> 
-        ( (u32, u32), (u32, u32), Vec<Vec<u32>>) {
-        let mut tiles: Vec<Vec<u32>> = vec![];
-        let mut start: (u32, u32) = (0, 0);
-        let mut end: (u32, u32) = (0, 0);
+        Result<(Vec<Vec<u32>>, Position, Position), &'static str> {
 
+        let mut start: Option<Position> = None;
+        let mut goal: Option<Position> = None;
+
+        let mut tiles: Vec<Vec<u32>> = vec![];
         for (row, line) in tile_string.lines().enumerate() {
             let mut tile_line: Vec<u32> = vec![];
             for (col, tile) in line.chars().enumerate() {
                 match tile.to_digit(10) {
-                    Some(d) => {
-                        tile_line.push(d);
-                        if d == 2 {
-                            start = (row as u32, col as u32);
-                        } else if d == 3 {
-                            end = (row as u32, col as u32);
+                    Some(2) => {
+                        tile_line.push(2);
+                        match start {
+                            None => start = Some(Position::new(row as f64, col as f64)),
+                            _ => return Err("Map loading failed, more than one start tile."),
                         }
-                    }
+                    },
+                    Some(3) => {
+                        tile_line.push(3);
+                        match goal {
+                            None => goal = Some(Position::new(row as f64, col as f64)),
+                            _ => return Err("Map loading failed, more than one goal tile."),
+                        }
+                    },
+                    Some(d) => tile_line.push(d),
                     None => tile_line.push(0),
                 };
             }
@@ -57,6 +71,10 @@ impl Map {
             tiles.push(tile_line);
                 
         }
-        (start,end,tiles)
+
+        if let None = start { return Err("Map loading failed, no start tile.") }
+        if let None = goal { return Err("Map loading failed, no end tile.") }
+
+        Ok((tiles, start.unwrap(),goal.unwrap()))
     }
 }
