4f3222d087665c2c5887d3ea7271c24e4a933e84
Wilmer Finol Inciarte <wofinoli@gmail.com>
2020-07-18 18:11:05 -0500

    Refactor WinitState to WindowState

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@ mod map;
 mod window;
 
 use map::Map;
-use window::WinitState;
+use window::WindowState;
+use winit::{
+    event::*,
+    event_loop::ControlFlow,
+};
+
 
 pub fn run() {
 
@@ -12,6 +17,19 @@ pub fn run() {
         Err(e) => println!("{}", e),
     }
 
-    let window_state = WinitState::default();
-    WinitState::run(window_state);
+    let (event_loop, window) = WindowState::default().expect("Window creation failed.");
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => WindowState::handle_key_input(*input),
+                _ => (),
+            }
+            _ => ()
+        }
+    });
 }
--- src/window.rs
+++ src/window.rs
@@ -1,81 +1,52 @@
 use winit::{
-    event::{Event, WindowEvent, KeyboardInput},
-    event_loop::{ControlFlow, EventLoop},
+    event::KeyboardInput,
+    event_loop::EventLoop,
     window::{Window, WindowBuilder},
     dpi::LogicalSize,
     error::OsError,
 };
 
 #[derive(Debug)]
-pub struct WinitState {
+pub struct WindowState {
     event_loop: EventLoop<()>,
     window: Window,
 }
 
+pub const WINDOW_NAME: &str = "Rustenstein";
 
-
-impl WinitState {
+impl WindowState {
     /// Constructs a new `EventsLoop` and `Window` pair.
     ///
     /// The specified title and size are used, other elements are default.
     /// ## Failure
     /// Window creating may fail, but this is unlikely
-    pub fn new<T: Into<String>>(title: T, size: LogicalSize<f64>) -> Result<Self, OsError> {
+    pub fn new<T: Into<String>>(title: T, size: LogicalSize<f64>) 
+            -> Result<(EventLoop<()>, Window), OsError> {
         let event_loop = EventLoop::new();
         let output = WindowBuilder::new()
             .with_title(title)
             .with_inner_size(size)
             .build(&event_loop);
-        output.map(|window| Self {
-            event_loop,
-            window,
-        })
+        
+        Ok( (event_loop, output.unwrap()) )
     }
 
-    /// Runs the window and event loop. 
-    /// This handles all inputs to the game.
-    pub fn run(state: WinitState) {
-        let event_loop = state.event_loop;
-
-        event_loop.run( move |event, _, control_flow| {
-            *control_flow = ControlFlow::Wait;
-
-            match event {
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => *control_flow = ControlFlow::Exit,
-                Event::WindowEvent {
-                    event: WindowEvent::KeyboardInput { input, .. },
-                    ..
-                } => handle_key_input(input),
-                _ => (),
-            }
-        });
+    /// Takes keyboard input and directs it to the
+    /// appropriate place.
+    pub fn handle_key_input(input: KeyboardInput) {
+        println!("{:?}", input);
     }
 
-}
-
-pub const WINDOW_NAME: &str = "Rustenstein";
-
-impl Default for WinitState {
     /// Makes an 640x480 windwow with `WINDOW_NAME` as the title.
     /// ## Panics
     /// If an `OsError` occurs.
-    fn default() -> WinitState {
-        WinitState::new(
+    pub fn default() -> Result<(EventLoop<()>, Window ), OsError> {
+        WindowState::new(
             WINDOW_NAME,
             LogicalSize {
                 width: 640.0,
                 height: 480.0,
             },
         )
-        .expect("Could not create window.")
     }
-}
-
-/// Takes keyboard input and directs it to the
-/// appropriate place.
-fn handle_key_input(input: KeyboardInput) {
-    println!("{:?}", input);
 }
